e803b41d648c62621893112ce0f0709c329bd6d5
Johan Melin <johan.melin@picsmart.se>
2023-05-14 10:21:33 +0200

    add react-native-dotenv

--- app/index.js
+++ app/index.js
@@ -5,16 +5,17 @@ import GradientBackground from "../src/components/GradientBackground";
 import TextFont from "../src/components/TextFont";
 import axios from 'axios';
 import WeatherCard from "../src/components/WeatherCard";
+import Constants from 'expo-constants';
+import { OPENWEATHER_API } from '@env';
 
 const MainPage = () => {
   const router = useRouter();
   const handlePress = () => {
     router.push("details");
   };
-  const apiKey = process.env.OPENWEATHER_API;
   async function getWeather() {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`);
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${OPENWEATHER_API}`);
       console.log(response);
     } catch (error) {
       console.error(error);

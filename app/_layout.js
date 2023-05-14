b5aa5891fd03294dee3d093b70041f7bd8786b6d
Johan Melin <johan.melin@picsmart.se>
2023-05-14 11:53:21 +0200

    add react query

--- app/_layout.js
+++ app/_layout.js
@@ -2,33 +2,46 @@ import { Stack } from "expo-router";
 import { Pressable } from "react-native";
 import { useRouter } from "expo-router";
 import TextFont from "../src/components/TextFont";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+//import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
 
 export default function RootLayout() {
   const router = useRouter();
   const handlePress = () => {
     router.back();
   };
+  const queryClient = new QueryClient({ 
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 10,
+      }
+    }
+  });
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="details"
-        options={{
-          headerTitle: "",
-          headerTransparent: true,
-          headerShadowVisible: false,
-          headerLeft: (props) => (
-            <Pressable onPress={handlePress}>
-              <TextFont>{"< Back"}</TextFont>
-            </Pressable>
-          ),
-        }}
-      ></Stack.Screen>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="details"
+          options={{
+            headerTitle: "",
+            headerTransparent: true,
+            headerShadowVisible: false,
+            headerLeft: (props) => (
+              <Pressable onPress={handlePress}>
+                <TextFont>{"< Back"}</TextFont>
+              </Pressable>
+            ),
+          }}
+        ></Stack.Screen>
       </Stack>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
--- app/index.js
+++ app/index.js
@@ -3,25 +3,27 @@ import { useRouter } from "expo-router";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import GradientBackground from "../src/components/GradientBackground";
 import TextFont from "../src/components/TextFont";
-import axios from 'axios';
 import WeatherCard from "../src/components/WeatherCard";
-import Constants from 'expo-constants';
-import { OPENWEATHER_API } from '@env';
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { getWeather, getWeatherDetails } from "../src/api/weather";
 
 const MainPage = () => {
   const router = useRouter();
   const handlePress = () => {
     router.push("details");
   };
-  async function getWeather() {
-    try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${OPENWEATHER_API}`);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+  const weatherQuery = useQuery({
+    queryKey: ["currentWeather"],
+    queryFn: () => getWeather,
+  })
+  const queryClient = useQueryClient()
+  function detailsQuery() {
+    queryClient.prefetchQuery({
+      queryKey: ["weatherDetails"],
+      queryFn: () => getWeatherDetails,
+    })
   }
-  getWeather();
+  //console.log(weatherQuery.fetchStatus, weatherQuery.status);
 
   return (
     <GradientBackground>
